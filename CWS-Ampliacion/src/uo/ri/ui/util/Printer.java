36ededeb32ca4ed8171e2b61bdd915b0f421c5f6
Elmer <UO257192@uniovi.es>
2019-10-22 15:23:31 +0200

    gf

--- CWS-Ampliacion/src/uo/ri/ui/util/Printer.java
+++ CWS-Ampliacion/src/uo/ri/ui/util/Printer.java
@@ -22,15 +22,15 @@ public class Printer {
 	public static void printInvoice(InvoiceDto invoice) {
 
 		double importeConIVa = invoice.total;
-		double iva =  invoice.vat;
+		double iva = invoice.vat;
 		double importeSinIva = importeConIVa / (1 + iva / 100);
 
-		Console.printf("Factura nº: %d%n", 				invoice.number );
-		Console.printf("\tFecha: %1$td/%1$tm/%1$tY%n", 	invoice.date);
-		Console.printf("\tTotal: %.2f €%n", 			importeSinIva);
-		Console.printf("\tIva: %.1f %% %n", 			invoice.vat );
-		Console.printf("\tTotal con IVA: %.2f €%n", 	invoice.total );
-		Console.printf("\tEstado: %s%n", 				invoice.status );
+		Console.printf("Factura nº: %d%n", invoice.number);
+		Console.printf("\tFecha: %1$td/%1$tm/%1$tY%n", invoice.date);
+		Console.printf("\tTotal: %.2f €%n", importeSinIva);
+		Console.printf("\tIva: %.1f %% %n", invoice.vat);
+		Console.printf("\tTotal con IVA: %.2f €%n", invoice.total);
+		Console.printf("\tEstado: %s%n", invoice.status);
 	}
 
 	public static void printPaymentMeans(List<PaymentMeanDto> medios) {
@@ -39,96 +39,56 @@ public class Printer {
 
 		Console.printf("\t%s \t%-8.8s \t%s %n", "ID", "Tipo", "Acumulado");
 		for (PaymentMeanDto medio : medios) {
-			printPaymentMean( medio );
+			printPaymentMean(medio);
 		}
 	}
 
 	private static void printPaymentMean(PaymentMeanDto medio) {
-		Console.printf("\t%s \t%-8.8s \t%s %n", 
-				medio.id,
-				medio.getClass().getName(),  // not the best...
-				medio.accumulated
-				);
+		Console.printf("\t%s \t%-8.8s \t%s %n", medio.id, medio.getClass().getName(), // not the best...
+				medio.accumulated);
 	}
 
 	public static void printRepairing(BreakdownDto rep) {
 
-		Console.printf("\t%d \t%-40.40s \t%td/%<tm/%<tY \t%-12.12s \t%.2f%n",  
-				rep.id
-				, rep.description 
-				, rep.date
-				, rep.status
-				, rep.total
-				);
+		Console.printf("\t%d \t%-40.40s \t%td/%<tm/%<tY \t%-12.12s \t%.2f%n", rep.id, rep.description, rep.date,
+				rep.status, rep.total);
 	}
 
 	public static void printMechanic(MechanicDto m) {
 
-		Console.printf("\t%d %-10.10s %-25.25s %-25.25s%n",  
-				m.id
-				, m.dni
-				, m.name
-				, m.surname
-				);
+		Console.printf("\t%d %-10.10s %-25.25s %-25.25s%n", m.id, m.dni, m.name, m.surname);
 	}
 
 	public static void printContractCategory(ContractCategoryDto c) {
 
-		Console.printf("\t%d %-20.20s %7.2f %3.2f%n",  
-				c.id
-				, c.name
-				, c.trieniumSalary
-				, c.productivityPlus
-				);
+		Console.printf("\t%d %-20.20s %7.2f %3.2f%n", c.id, c.name, c.trieniumSalary, c.productivityPlus);
 
 	}
 
 	public static void printContractType(ContractTypeDto t) {
 
-		Console.printf("\t%d %-20.20s %2d%n",  
-				t.id
-				, t.name
-				, t.compensationDays
-				);
+		Console.printf("\t%d %-20.20s %2d%n", t.id, t.name, t.compensationDays);
 
 	}
 
 	public static void printContract(ContractDto c) {
 
-		Console.printf(
-				"\t%d %-10.10s %-20.20s %-20.20s %8.8s %td/%tm/%tY %7.2f %7.2f %td/%tm/%tY %n",  
-				c.id
-				, c.dni
-				, c.categoryName
-				, c.typeName
-				, c.status
-				, c.startDate, c.startDate, c.startDate
-				, c.yearBaseSalary
-				, c.compensation
-				, c.endDate, c.endDate, c.endDate
-				);
+		Console.printf("\t%d %-10.10s %-20.20s %-20.20s %8.8s %td/%tm/%tY %7.2f %7.2f %td/%tm/%tY %n", c.id, c.dni,
+				c.categoryName, c.typeName, c.status, c.startDate, c.startDate, c.startDate, c.yearBaseSalary,
+				c.compensation, c.endDate, c.endDate, c.endDate);
 
 	}
 
 	public static void printPayrollSummary(PayrollDto p) {
 
-		Console.printf("\t%d %tm/%tY %7.2f%n",  
-				p.id
-				, p.date, p.date
-				, p.netTotal
-				);
+		Console.printf("\t%d %tm/%tY %7.2f%n", p.id, p.date, p.date, p.netTotal);
 
 	}
 
 	public static void printPayrollDetail(PayrollDto p) {
 
-		Console.printf("\t%d %tm/%tY %7.2f%n %7.2f%n %7.2f%n",  
-				p.id
-				, p.date, p.date
-				, p.grossTotal
-				, p.discountTotal
-				, p.netTotal
-				);
+		Console.printf("\t%d %tm/%tY %7.2f%n %7.2f%n %7.2f%n", p.id, p.date, p.date, p.grossTotal, p.discountTotal,
+				p.netTotal);
 		Console.println("Abonos");
 		Console.printf("/t Salario base: %7.2f%n", p.baseSalary);
 		Console.printf("/t        extra: %7.2f%n", p.extraSalary);
@@ -138,54 +98,33 @@ public class Printer {
 		Console.printf("/t         IRPF: %7.2f%n", p.irpf);
 		Console.printf("/t    S. social: %7.2f%n", p.socialSecurity);
 	}
-	
+
 	public static void printCertifiedMechanic(CertificateDto c) {
 
-		Console.printf("%d\t%-10.10s %-25.25s %-25.25s\n",
-				c.mechanic.id
-				, c.mechanic.dni
-				, c.mechanic.name
-				, c.vehicleType.name
-			);
+		Console.printf("%d\t%-10.10s %-25.25s %-25.25s\n", c.mechanic.id, c.mechanic.dni, c.mechanic.name,
+				c.vehicleType.name);
 	}
 
 	public static void printWorkOrderDetail(WorkOrderDto wo) {
 
-		Console.printf("%d for vehicle %s\n\t%-25.25s\n\t%tm/%<td/%<tY\n\t%s\n",
-				wo.id
-				, wo.vehicleId
-				, wo.description
-				, wo.date
-				, wo.status
-			);
+		Console.printf("%d for vehicle %s\n\t%-25.25s\n\t%tm/%<td/%<tY\n\t%s\n", wo.id, wo.vehicleId, wo.description,
+				wo.date, wo.status);
 	}
 
 	public static void printVehicleDetail(VehicleDto v) {
 
-		Console.printf("%d\t%-8.8s\t%s\t%s\n",
-				v.id
-				, v.plate
-				, v.make
-				, v.model
-			);
+		Console.printf("%d\t%-8.8s\t%s\t%s\n", v.id, v.plate, v.make, v.model);
 	}
-	
+
 	public static void printTrainingForMechanic(TrainingForMechanicRow row) {
 
-		Console.printf("\t%-20.20s\t%d\t%d\n"
-					, row.vehicleTypeName
-					, row.enrolledHours
-					, row.attendedHours
-		);
+		Console.printf("\t%-20.20s\t%d\t%d\n", row.vehicleTypeName, row.enrolledHours, row.attendedHours);
 
 	}
 
 	public static void printTrainingHoursRow(TrainingHoursRow r) {
-
-		Console.printf("%-20.20s\t%-30.30s\t%d hours\n"
-				, r.vehicleTypeName
-				, r.mechanicFullName
-				, r.enrolledHours);
+		Console.println((r.vehicleTypeName.equals("") ? r.vehicleTypeName: r.vehicleTypeName+"\n") + "\t"+r.mechanicFullName + "\t" +  r.enrolledHours);
+		//Console.printf("%-20.20s %-30.30s\t%d hours\n", r.vehicleTypeName.equals("") ? "\n": r.vehicleTypeName+"\n", r.mechanicFullName, r.enrolledHours);
 	}
 
 }
